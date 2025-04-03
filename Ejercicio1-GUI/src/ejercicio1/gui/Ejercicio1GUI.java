2084ac2d8cc648dd5c3b5b18937a9ed4e34ee400
Avril Miniza <avril.miniza470@hotmail.com>
2025-04-02 22:16:11 -0500

    Prueba de cambios en git

---
+++ Ejercicio1-GUI/src/ejercicio1/gui/Ejercicio1GUI.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1.gui;
+
+/**
+ *
+ * @author Avril
+ */
+public class Ejercicio1GUI {
+
+    public static void main(String[] args) {
+        // Declaración de variables
+        String mensaje = "¡Hola, Avril!";
+        int edad = 18;
+        double altura = 1.65;
+
+        // Imprimir el valor de las variables
+        System.out.println(mensaje);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura + " metros");
+    }
+}
