75b2d50d14da08692f9bc492c8970b38d0abe6be
Avril Miniza <avril.miniza470@hotmail.com>
2025-04-02 23:26:03 -0500

    Interfaces del ejercicio 1 hechas

--- Ejercicio1-GUI/src/ejercicio1/gui/Ejercicio1GUI.java
+++ Ejercicio1-GUI/src/ejercicio1/gui/Ejercicio1GUI.java
@@ -11,14 +11,6 @@ package ejercicio1.gui;
 public class Ejercicio1GUI {
 
     public static void main(String[] args) {
-        // Declaración de variables
-        String mensaje = "¡Hola, Avril!";
-        int edad = 18;
-        double altura = 1.65;
-
-        // Imprimir el valor de las variables
-        System.out.println(mensaje);
-        System.out.println("Edad: " + edad);
-        System.out.println("Altura: " + altura + " metros");
+        
     }
 }
